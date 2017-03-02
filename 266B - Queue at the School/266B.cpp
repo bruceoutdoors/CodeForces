35735d42e2e42477581fc530405316eccabb051c
Lee Zhen Yong <bruceoutdoors@gmail.com>
2017-03-02 16:59:00 +0800

    266B - Queue at the School

---
+++ 266B - Queue at the School/266B.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+	int n, t; cin >> n >> t;
+	
+	char a;
+	char q[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		q[i] = a;
+	}
+	
+	for (int i = 0; i < t; ++i) {
+		for (int j = n - 2; j >= 0; --j) {
+			if (q[j] == 'B' && q[j+1] == 'G') {
+				q[j]   = 'G';
+				q[j+1] = 'B';
+				--j;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) cout << q[i];
+}
