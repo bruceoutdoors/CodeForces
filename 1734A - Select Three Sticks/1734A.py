3eee66955a6767d60d7fad34709d2d6b3fd21a6c
Lee Zhen Yong <bruceoutdoors@gmail.com>
2022-09-27 10:52:28 +0800

    wow so short

--- 1734A - Select Three Sticks/1734A.py
+++ 1734A - Select Three Sticks/1734A.py
@@ -1,15 +1,9 @@
 # python3
-from statistics import median
-from sys import maxsize
 t = int(input())
 
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
     a = sorted(a)
-    min_diffs = maxsize
-    for i in range(n - 2):
-        seg = a[i:i+3]
-        mid = median(seg)
-        min_diffs = min(min_diffs, sum([abs(mid - x) for x in seg]))
+    min_diffs = min([a[i+2] - a[i] for i in range(n - 2)])
     print(min_diffs)
